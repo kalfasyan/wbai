ea1e3b2ceed3c9e0ea92ad3bff732435dd378a66
Yannis <kalfasyan@gmail.com>
2021-01-06 16:59:31 +0100

    added cleaning of datasets

--- wingbeat_datasets.py
+++ wingbeat_datasets.py
@@ -1,8 +1,13 @@
 import torch
-from torch.utils.data import Dataset, ConcatDataset
+from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
+from torchvision import transforms
 from sklearn import preprocessing
 from utils import open_wingbeat, make_dataset_df, butter_bandpass_filter
+import librosa
+import numpy as np
+from scipy import signal as sg
+from tqdm import tqdm
 
 class WingbeatsDataset(Dataset):
     """Wingbeats dataset."""
@@ -16,7 +21,7 @@ class WingbeatsDataset(Dataset):
         """
         self.dsname = dsname
         self.files, self.labels, self.lbl2files = make_dataset_df(dsname, verbose=True)
-        if custom_label is not None:
+        if len(custom_label):
             self.labels = [custom_label for _ in self.labels]  
             print(f"Label(s) changed to {custom_label}")
         else: 
@@ -33,27 +38,91 @@ class WingbeatsDataset(Dataset):
         fname = self.files[idx]
         label = self.labels[idx]
         wingbeat = open_wingbeat(fname, plot=False)
-        sample = {'x': wingbeat, 'y': label, 'path': str(fname)}
+        sample = {'x': wingbeat, 'y': label, 'path': str(fname), 'idx': idx}
 
         if self.transform:
             sample = self.transform(sample)
 
-        return sample['x'], sample['y'], sample['path']
+        return sample['x'], sample['y'], sample['path'], sample['idx']
 
     def onehotlabels(self):
         le = preprocessing.LabelEncoder()
         self.labels = le.fit_transform(self.labels)
         self.labels = F.one_hot(torch.as_tensor(self.labels))
 
-class FilterWingbeat(object):
-    def __init__(self, filter_type='bandpass'):
-        self.filter_type = filter_type
+class ValidateWingbeat(object):
+    def __init__(self, setting='default'):
+        self.setting = setting
 
     def __call__(self, sample):
         wingbeat = sample['x']
         label = sample['y']
-        if self.filter_type == 'bandpass':
+
+        if self.setting == 'default':
+            wingbeat = torch.from_numpy(butter_bandpass_filter(wingbeat)).float()
+            _, wingbeat = sg.welch(wingbeat, fs=8000, scaling='density', window='hanning', nfft=8192, nperseg=256, noverlap=128+64)
+            wingbeat = preprocessing.normalize(wingbeat.reshape(1,-1), norm='l2')
+            score = np.sum(wingbeat)
+        else:
+            raise NotImplementedError('!')
+
+        return {'x': wingbeat, 'y': label, 'path': sample['path'], 
+                'idx': sample['idx'], 'score': score}
+
+class FilterWingbeat(object):
+    def __init__(self, setting='bandpass'):
+        self.setting = setting
+
+    def __call__(self, sample):
+        wingbeat = sample['x']
+        label = sample['y']
+
+        if self.setting == 'bandpass':
             wingbeat = torch.from_numpy(butter_bandpass_filter(wingbeat)).float()
         else:
             raise NotImplementedError('!')
-        return {'x': wingbeat, 'y': label, 'path': sample['path']}
+
+        return {'x': wingbeat, 'y': label, 'path': sample['path'], 'idx': sample['idx']}
+
+class TransformWingbeat(object):
+    def __init__(self, setting=''):
+        self.setting = setting
+        assert len(self.setting), "Please provide a transformation setting."
+
+    def __call__(self, sample):
+        
+        wingbeat = sample['x'].numpy().squeeze()
+        label = sample['y']
+        
+        if self.setting == 'stft':
+            wingbeat = librosa.stft(wingbeat, n_fft = 256, hop_length = int(256/6))
+            wingbeat = librosa.amplitude_to_db(np.abs(wingbeat))
+            wingbeat = np.flipud(wingbeat)
+        elif self.setting.startswith('psd'):
+            _, wingbeat = sg.welch(wingbeat, fs=8000, scaling='density', window='hanning', nfft=8192, nperseg=256, noverlap=128+64)
+            if self.setting == 'psdl1':
+                wingbeat = preprocessing.normalize(wingbeat.reshape(1,-1), norm='l1')
+            elif self.setting == 'psdl2':
+                wingbeat = preprocessing.normalize(wingbeat.reshape(1,-1), norm='l2')
+
+        return {'x': wingbeat, 'y': label, 'path': sample['path'], 'idx': sample['idx']}
+
+def clean_wingbeatsdataset_inds(name="Melanogaster_RL/Y", batch_size=16, num_workers=16):
+    dataset = WingbeatsDataset(name, transform=TransformWingbeat(setting='psdl2'))
+    dloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+
+    all_inds_valid = torch.tensor([])
+    for i, (x_batch,y_batch,path_batch,idx) in enumerate(tqdm(dloader)):
+
+        xb_sums = x_batch.squeeze().sum(axis=1)
+        xb_valid = torch.where(xb_sums<25,xb_sums,torch.zeros(xb_sums.shape))
+        inds_valid = idx[xb_valid.nonzero().squeeze()]
+        all_inds_valid = torch.cat((all_inds_valid, inds_valid),0)
+
+    return list(all_inds_valid.numpy().astype(int))
+
+def clean_wingbeatdataset(name="Melanogaster_RL/Y", custom_label=0, transform=transforms.Compose([FilterWingbeat(setting='bandpass')])):
+    dset = WingbeatsDataset(name, custom_label=custom_label, transform=transform)
+    inds = clean_wingbeatsdataset_inds(name=name)
+    dset = torch.utils.data.Subset(dset, inds)
+    return dset
--- wingbeat_models.py
+++ wingbeat_models.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class Conv1dNet(nn.Module):
+class Conv1dNetRAW(nn.Module):
     def __init__(self):
-        super(Conv1dNet, self).__init__()
+        super(Conv1dNetRAW, self).__init__()
         self.conv1 = nn.Conv1d(1, 16, 3)
         self.bn1 = nn.BatchNorm1d(16)
         self.pool1 = nn.MaxPool1d(2)
@@ -76,6 +76,77 @@ class Conv1dNet(nn.Module):
         # print(f"view: {x.shape}")
         x = self.fc1(x)
         # print(f"fc1: {x.shape}")
+        return x
+
+class Conv1dNetPSD(nn.Module):
+    def __init__(self):
+        super(Conv1dNetPSD, self).__init__()
+        self.conv1 = nn.Conv1d(1, 16, 3)
+        self.bn1 = nn.BatchNorm1d(16)
+        self.pool1 = nn.MaxPool1d(2)
+
+        self.conv2 = nn.Conv1d(16, 32, 3)
+        self.bn2 = nn.BatchNorm1d(32)
+        self.pool2 = nn.MaxPool1d(2)
+
+        self.conv3 = nn.Conv1d(32, 64, 3)
+        self.bn3 = nn.BatchNorm1d(64)
+        self.pool3 = nn.MaxPool1d(2)
+
+        self.conv4 = nn.Conv1d(64, 128, 3)
+        self.bn4 = nn.BatchNorm1d(128)
+        self.pool4 = nn.MaxPool1d(2) 
+
+        self.dropout = nn.Dropout()
+        self.avgPool = nn.AvgPool1d(127)
+        self.fc1 = nn.Linear(256, 2)
+
+    def forward(self, x):
+        # print("######")
+
+        x = self.conv1(x)
+        # print(f"conv1: {x.shape}")
+        x = F.relu(self.bn1(x))
+        # print(f"relu2: {x.shape}")
+        x = self.pool1(x)
+        # print(f"pool1: {x.shape}")
+
+        x = self.conv2(x)
+        # print(f"conv2: {x.shape}")
+        x = F.relu(self.bn2(x))
+        # print(f"relu2: {x.shape}")
+        x = self.pool2(x)
+        # print(f"pool2: {x.shape}")
+
+        x = self.conv3(x)
+        # print(f"conv3: {x.shape}")
+        x = F.relu(self.bn3(x))
+        # print(f"relu3: {x.shape}")
+        x = self.pool3(x)
+        # print(f"pool3: {x.shape}")
+
+        x = self.conv4(x)
+        # print(f"conv4: {x.shape}")
+        x = F.relu(self.bn4(x))
+        # print(f"relu4: {x.shape}")
+        x = self.pool4(x)
+        # print(f"pool4: {x.shape}")
+
+        # x = self.conv5(x)
+        # print(f"conv5: {x.shape}")
+        # x = F.relu(self.bn5(x))
+        # print(f"relu5: {x.shape}")
+        # x = self.pool5(x)
+        # print(f"pool5: {x.shape}")
+
+        x = self.dropout(x)
+        x = self.avgPool(x)
+        # print(f"avgPool: {x.shape}")
+        # x = x.permute(0, 2, 1) #change the 512x1 to 1x512
+        x = x.view(x.shape[0], -1)
+        # print(f"view: {x.shape}")
+        x = self.fc1(x)
+        # print(f"fc1: {x.shape}")
         # x = F.log_softmax(x, dim = 1)
         # print(f"log_softmax: {x.shape}")
         return x
@@ -83,7 +154,7 @@ class Conv1dNet(nn.Module):
 # Credit: Bjarte Mehus Sunde from Github
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+    def __init__(self, patience=7, verbose=False, delta=0, path='data_created/checkpoint.pt', trace_func=print):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -115,7 +186,8 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.verbose:
+                self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
